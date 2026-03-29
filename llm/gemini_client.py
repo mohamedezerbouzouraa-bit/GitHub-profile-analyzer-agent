93580f29cb5020ee166714b12bcd3e9537f535b8
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-29 22:36:34 +0200

    Update gemini_client.py

--- llm/gemini_client.py
+++ llm/gemini_client.py
@@ -2,7 +2,6 @@ from google import genai
 from config.settings import GEMINI_API_KEY, MODEL
 
 client = genai.Client(api_key=GEMINI_API_KEY)
-
 def generate(prompt: str):
     response = client.models.generate_content(
         model=MODEL,
